2faf1cb35d2da4496a1e058480cbed4cc0205ee7
hardcommitoneself <hardcommitoneself@gmail.com>
2024-11-07 00:46:40 +0700

    feat: add several blank pages

---
+++ app/Filament/Pages/Shortener/Shorteners.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Pages\Shortener;
+
+use Filament\Pages\Page;
+
+class Shorteners extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-circle-stack';
+
+    protected static string $view = 'filament.pages.shortener.shorteners';
+
+    protected static ?string $navigationGroup = 'Shortener';
+
+    protected static ?int $navigationSort = 1;
+}
