dbcbaecc991598be77e65c29b589423dd126d079
AvalonRychmon <35082441+AvalonRychmon@users.noreply.github.com>
2024-11-12 17:43:46 -0600

    Updated: Table Format

--- app/Filament/Pages/Shorteners.php
+++ app/Filament/Pages/Shorteners.php
@@ -19,6 +19,7 @@ use Filament\Tables\Columns\ToggleColumn;
 use Illuminate\Support\Facades\Auth;
 use Filament\Tables\Actions\Action;
 use Filament\Tables\Columns\IconColumn;
+use Illuminate\Support\HtmlString;
 
 class Shorteners extends Page implements HasTable
 {
@@ -35,7 +36,6 @@ class Shorteners extends Page implements HasTable
     public function table(Table $table): Table
     {
         return $table
-            ->description('Websites')
             ->query(Shortener::activeShorteners())
             ->columns([
                 IconColumn::make('status')
@@ -45,13 +45,19 @@ class Shorteners extends Page implements HasTable
                         false => 'gray'
                     }),
                 TextColumn::make('name')
-                    ->url(fn (Shortener $record) => $record->referral)
-                    ->openUrlInNewTab()
+                    ->url(fn (Shortener $record) => $record->referral, true)
                     ->searchable()
                     ->sortable(),
                 TextColumn::make('cpm')
                     ->label('CPM')
                     ->money('usd')
+                    ->alignCenter()
+                    ->sortable(),
+                TextColumn::make('views')
+                    ->getStateUsing(function (Shortener $record) {
+                        return $record->setting()->views ?? $record->views;
+                    })
+                    ->alignCenter()
                     ->sortable(),
                 TextColumn::make('api_key')
                     ->label('API Key')
@@ -59,52 +65,54 @@ class Shorteners extends Page implements HasTable
                     ->copyable(fn (Shortener $record) => $record->isSettingExisted())
                     ->color(fn (Shortener $record) => $record->isSettingExisted() ? 'primary' : 'warning')
                     ->getStateUsing(function (Shortener $record) {
-                        return $record->setting()->api_key ?? 'Please active this shortener';
-                    }),
-                TextColumn::make('views')
-                    ->getStateUsing(function (Shortener $record) {
-                        return $record->setting()->views ?? $record->views;
+                       return $record->setting()->api_key ?? 'Add API Key to Enable';
                     })
-                    ->sortable(),
+                    ->icon(fn (Shortener $record) => $record->isSettingExisted() ? null : 'heroicon-o-cog-6-tooth')
+                    ->limit(21)
+                    ->extraHeaderAttributes(['class' => 'w-8'])
+                    ->action(
+                        Action::make('Active')
+                            ->modalHeading('Add API Key')
+                            ->modalDescription(function (Shortener $record) { 
+                                return new HtmlString('Get your API Key from <b><a href="'. $record->referral .'" target="_blank">'. $record->name .'</a></b>\'s Developers API page.' ); 
+                            })
+                            ->modalWidth(MaxWidth::Large)
+                            ->form([
+                                TextInput::make('api_key')
+                                    ->label('API Key')
+                                    ->required()
+                                    ->maxLength(255),
+                                TextInput::make('views')
+                                    ->numeric()
+                                    ->default(1)
+                                    ->minValue(1)
+                                    ->required()
+                            ])
+                            ->action(function (Shortener $record, array $data) {
+                                $setting = new ShortenerSetting;
+            
+                                $setting->api_key = $data['api_key'];
+                                $setting->views = $data['views'];
+                                $setting->priority = Auth::user()->numberOfSettings() + 1;
+                                $setting->user_id = Auth::user()->id;
+                                $setting->shortener_id = $record->id;
+            
+                                $setting->save();
+            
+                                Notification::make() 
+                                    ->title('Success')
+                                    ->icon('heroicon-o-check-circle')
+                                    ->success()
+                                    ->body($record->name.' updated successfully')
+                                    ->send();
+                            })
+                            ->closeModalByClickingAway(false)
+                            ->visible(fn (Shortener $record) => !$record->isSettingExisted())
+                    )
             ])
             ->actions([
-                Action::make('Active')
-                    ->label('Active this shortener')
-                    ->iconButton()
-                    ->icon('heroicon-o-rocket-launch')
-                    ->modalWidth(MaxWidth::Large)
-                    ->form([
-                        TextInput::make('api_key')
-                            ->label('API Key')
-                            ->placeholder('***********')
-                            ->required()
-                            ->maxLength(255),
-                        TextInput::make('views')
-                            ->numeric()
-                            ->minValue(1)
-                            ->required()
-                    ])
-                    ->action(function (Shortener $record, array $data) {
-                        $setting = new ShortenerSetting;
-
-                        $setting->api_key = $data['api_key'];
-                        $setting->views = $data['views'];
-                        $setting->priority = Auth::user()->numberOfSettings() + 1;
-                        $setting->user_id = Auth::user()->id;
-                        $setting->shortener_id = $record->id;
-
-                        $setting->save();
-
-                        Notification::make() 
-                            ->title('Success')
-                            ->icon('heroicon-o-check-circle')
-                            ->success()
-                            ->body('Activate shortener successfully')
-                            ->send();
-                    })
-                    ->visible(fn (Shortener $record) => !$record->isSettingExisted()),
                 Action::make('Activate/Deactivate')
-                    ->label(fn (Shortener $record) => $record->setting()->status ? 'Deactivate this shortener' : 'Activate this shortener')
+                    ->label(fn (Shortener $record) => $record->setting()->status ? 'Disable '.$record->name :  'Enable '.$record->name)
                     ->iconButton()
                     ->color(fn (Shortener $record) => $record->setting()->status ? 'warning' : 'primary')
                     ->icon(fn (Shortener $record) => $record->setting()->status ? 'heroicon-o-signal-slash' : 'heroicon-o-signal')
@@ -120,26 +128,33 @@ class Shorteners extends Page implements HasTable
                             ->title('Success')
                             ->icon('heroicon-o-check-circle')
                             ->success()
-                            ->body(!$record->setting()->status ? 'Deactivate shortener successfully' : 'Activate shortener successfully')
+                            ->body(!$record->setting()->status ? $record->name.' has been disabled' : $record->name.' has been enabled')
                             ->send();
                     })
                     ->visible(fn (Shortener $record) => $record->isSettingExisted()),
                 Action::make('Edit')
-                    ->label('Edit this setting')
+                    ->modalHeading('Edit API Key')
+                    ->modalDescription(function (Shortener $record) { 
+                        return new HtmlString('Get your API Key from <b><a href="'. $record->referral .'" target="_blank">'. $record->name .'</a></b>\'s Developers API page.' ); 
+                    })
+                    ->modalWidth(MaxWidth::Large)
+                    ->label(function (Shortener $record) { 
+                                return 'Edit '.$record->name;
+                    })
                     ->iconButton()
                     ->icon('heroicon-o-cog-6-tooth')
-                    ->modalWidth(MaxWidth::Large)
                     ->fillForm(fn (Shortener $record) => $record->setting()->only(['api_key', 'views']))
                     ->form([
                         TextInput::make('api_key')
                             ->label('API Key')
-                            ->placeholder('***********')
                             ->required()
                             ->maxLength(255),
                         TextInput::make('views')
                             ->numeric()
+                            ->default(1)
                             ->minValue(1)
                             ->required()
+                            ->maxLength(255)
                     ])
                     ->action(function (Shortener $record, array $data) {
                         $setting = $record->setting();
