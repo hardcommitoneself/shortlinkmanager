fe2feb3b47da5c26319c47ebbabfe90d3d82176a
hardcommitoneself <hardcommitoneself@gmail.com>
2024-12-04 03:00:58 +0800

    fix

--- app/Models/Visit.php
+++ app/Models/Visit.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Visit extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'is_completed',
+    ];
 }
